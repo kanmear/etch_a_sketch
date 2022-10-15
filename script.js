c0588f4585a355810f13f620de26be135c4df567
kanmear <kanmear_github@gmail.com>
2022-10-15 21:41:42 +0300

    feat: minimal drawing impl

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const gridContainer = document.querySelector('.grid-container')
 const slider = document.querySelector('.slider')
 const sliderValue = document.querySelector('.slider-value')
 
+let isMouseDown = false
+
 function generateGrid(size) {
     document.querySelectorAll('.grid-row').forEach(c => c.remove())
     document.querySelectorAll('.cell').forEach(c => c.remove())
@@ -26,5 +28,19 @@ slider.addEventListener('input', () => {
     sliderValue.innerHTML = slider.value + 'x' + slider.value
 })
 
+document.body.onmousedown = () => isMouseDown = true
+document.body.onmouseup = () => isMouseDown = false
+document.body.onmouseleave = () => isMouseDown = false
+
+gridContainer.addEventListener('mouseover', (event) => {
+    let element = event.target
+    if (isMouseDown) {
+        element.classList.add('cell-active')
+    }
+})
+
+document.body.addEventListener('dragstart', (element) => element.preventDefault())
+
 //testing
 generateGrid(12)
+sliderValue.innerHTML = slider.value + 'x' + slider.value
