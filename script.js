b62a9463034398d4a811f561763db22123206e01
kanmear <kanmear_github@gmail.com>
2022-10-15 20:24:41 +0300

    refactor: optimized cell dynamic sizing

--- script.js
+++ script.js
@@ -3,30 +3,28 @@ const slider = document.querySelector('.slider')
 const sliderValue = document.querySelector('.slider-value')
 
 function generateGrid(size) {
+    document.querySelectorAll('.grid-row').forEach(c => c.remove())
     document.querySelectorAll('.cell').forEach(c => c.remove())
 
     const grid = []
-    const gridSide = getComputedStyle(gridContainer).height.slice(0, -2)
     for (let i = 0; i < size; i++) {
-        grid[i] = document.createElement('div')
-        grid[i].classList.add('grid-row')
+        let row = document.createElement('div')
+        row.className = 'grid-row'
+        // grid[i] = row
         for (let j = 0; j < size; j++) {
-            grid[i][j] = document.createElement('div')
-            grid[i][j].classList.add("cell")
-            grid[i].appendChild(grid[i][j])
+            let cell = document.createElement('div')
+            cell.className = 'cell'
+            row.appendChild(cell)
+            // grid[i][j] = elem
         }
-        gridContainer.appendChild(grid[i])
+        gridContainer.appendChild(row)
     }
-    document.querySelectorAll('.cell').forEach(c => {
-        c.style.height = gridSide / size + 'px'
-        c.style.width = gridSide / size + 'px'
-    })
 }
 
-//testing
-generateGrid(16)
-
-slider.addEventListener('change', () => {
+slider.addEventListener('input', () => {
     generateGrid(slider.value)
     sliderValue.innerHTML = slider.value + 'x' + slider.value
 })
+
+//testing
+generateGrid(12)
