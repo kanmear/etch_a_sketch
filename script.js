28b3022673ed1e7fe49376f3483092c93e41341a
kanmear <kanmear_github@gmail.com>
2022-10-15 15:05:27 +0300

    feat: minimal grid generation logic

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-function generateGrid() {
-    let grid = [];
-    for (let i = 0; i < 16; i++)
-        grid[i] = document.createElement('div');
+const gridContainer = document.querySelector('.grid-container')
+
+function generateGrid(size) {
+    const grid = []
+    for (let i = 0; i < size; i++) {
+        grid[i] = document.createElement('div')
+        grid[i].classList.add('grid-row')
+        for (let j = 0; j < size; j++) {
+            grid[i][j] = document.createElement('div')
+            grid[i][j].classList.add("cell")
+            grid[i].appendChild(grid[i][j])
+        }
+        gridContainer.appendChild(grid[i])
+    }
 }
+
+//testing
+generateGrid(16)
